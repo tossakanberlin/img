b648de4c10dc46c04aeceea7884c9122ba880b4a
Pakorn Sangpeth <camel4882@gmail.com>
2023-05-08 11:35:17 +0700

    feat: mailer api

---
+++ src/routes/api/email/+server.ts
@@ -0,0 +1,30 @@
+import { PRIVATE_EMAIL_PASS, PRIVATE_EMAIL_USER } from '$env/static/private';
+import { json, type RequestHandler } from '@sveltejs/kit';
+import nodemailer from 'nodemailer';
+
+export const GET: RequestHandler = async (event) => {
+	const body = await event.request.json();
+	const { from, to, subject, html } = body;
+
+	const transporter = nodemailer.createTransport({
+		host: 'smtp.gmail.com',
+		port: 587,
+		secure: false,
+		auth: {
+			user: PRIVATE_EMAIL_USER,
+			pass: PRIVATE_EMAIL_PASS
+		}
+	});
+
+	const info = await transporter.sendMail({
+		from, // sender address
+		to, // list of receivers
+		subject, // Subject line
+		html // html body
+	});
+
+	return json({
+		ok: true,
+		info
+	});
+};
